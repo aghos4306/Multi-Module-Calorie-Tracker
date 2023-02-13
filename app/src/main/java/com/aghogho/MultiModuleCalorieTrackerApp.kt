d9341f2c9090e405ad5059d68f1d98418ab302b2
Aghogho <aghogho.akpedeye@gmail.com>
2023-02-13 03:09:24 +0000

    configure dagger hilt and provide functions for preferences and sharedpreference

---
+++ app/src/main/java/com/aghogho/MultiModuleCalorieTrackerApp.kt
@@ -0,0 +1,8 @@
+package com.aghogho
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MultiModuleCalorieTrackerApp: Application() {
+}
---
+++ app/src/main/java/com/aghogho/multimodulecalorietracker/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.aghogho.multimodulecalorietracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.aghogho.core.data.preferences.DefaultPreferences
+import com.aghogho.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(app: Application): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+
+}
