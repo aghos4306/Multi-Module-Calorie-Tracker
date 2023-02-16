8c1e018b61a37b91d97c680ce830fd6aa3c320fe
Aghogho <aghogho.akpedeye@gmail.com>
2023-02-16 21:36:13 +0000

    built the logic to calculate meal nutrient use case

---
+++ tracker/tracker_domain/src/main/java/com/aghogho/tracker_domain/di/TrackerDomainModule.kt
@@ -0,0 +1,31 @@
+package com.aghogho.tracker_domain.di
+
+import com.aghogho.core.domain.preferences.Preferences
+import com.aghogho.tracker_domain.repository.TrackerRepository
+import com.aghogho.tracker_domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object TrackerDomainModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideTrackerUseCases(
+        repository: TrackerRepository,
+        preferences: Preferences
+    ): TrackerUseCases {
+        return TrackerUseCases(
+            trackFood = TrackFood(repository),
+            searchFood = SearchFood(repository),
+            getFoodsForDate = GetFoodsForDate(repository),
+            deleteTrackedFood = DeleteTrackedFood(repository),
+            calculateMealNutrient = CalculateMealNutrient(preferences)
+        )
+    }
+
+}
