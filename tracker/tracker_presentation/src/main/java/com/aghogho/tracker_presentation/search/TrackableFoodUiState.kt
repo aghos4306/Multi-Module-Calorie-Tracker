c45b2516584bf1a72f882eded296bd99c05b5be8
Aghogho <aghogho.akpedeye@gmail.com>
2023-02-24 17:48:47 +0200

    built the searchviewmodel and manage all uistates relating to this feature

---
+++ tracker/tracker_presentation/src/main/java/com/aghogho/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.aghogho.tracker_presentation.search
+
+import com.aghogho.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
